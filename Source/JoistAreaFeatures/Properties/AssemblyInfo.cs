231ce01acfde8c15abe9bac2c2be02407383bd54
Charles <charles.pool@trimble.com>
2022-03-22 15:08:25 -0500

    TSE-1365 >Update/branch examples for 2022 version >Cleanup project, props, target files >Move projects all under source root folder one level

---
+++ Source/JoistAreaFeatures/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Joist Area Features")]
+[assembly: AssemblyDescription("Tekla Structures Example")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Solutions USA Inc")]
+[assembly: AssemblyProduct("JoistAreaFeatures")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2021.0.0.0")]
+[assembly: AssemblyFileVersion("2021.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: Guid("145f494e-d938-4961-a404-b8aac159dbe0")]
